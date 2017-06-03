ac7888911c6c0829623e722fd7a692973c4024fd
kyli <kylidboy@gmail.com>
2017-06-03 16:32:04 +0800

    update

--- init-go.el
+++ init-go.el
@@ -6,17 +6,18 @@
 (use-package company-go :ensure t)
 (use-package go-eldoc :ensure t)
 (use-package go-guru :ensure t)
-(use-package go-dlv :ensure t)
+;; (use-package go-dlv :ensure t)
 (use-package flycheck-gometalinter
   :ensure t
   :config
   (progn
     (flycheck-gometalinter-setup)))
-
+(setenv "GOPATH" "/Users/kyli/Codes/golang")
 (setq company-tooltip-limit 20)
-(setq company-idle-delay .3)
+(setq company-idle-delay .8)
 (setq company-echo-delay 0)
 (setq company-begin-commands '(self-insert-command))
+(setq company-minimum-prefix-length 5)
 
 (custom-set-faces
  '(company-preview
@@ -34,16 +35,24 @@
    ((((type x)) (:inherit company-tooltip-selection :weight bold))
     (t (:inherit company-tooltip-selection)))))
 
+(setq gofmt-command "gofmt")
+;; skips 'vendor' directories and sets GO15VENDOREXPERIMENT=1
+(setq flycheck-gometalinter-vendor t)
+;; only show errors
+(setq flycheck-gometalinter-errors-only t)
+;; only run fast linters
+(setq flycheck-gometalinter-fast t)
+
 (add-hook 'go-mode-hook (lambda ()
                           (go-eldoc-setup)
-                          (setq gofmt-command "goimports")
+
                           (local-set-key (kbd "M-.") 'godef-jump)
-                          (add-hook 'before-save-hook 'gofmt-before-save)
-                          (add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
-                          (setq flycheck-gometalinter-fast t)
+                          (local-set-key (kbd "M-,") 'pop-tag-mark)
                           (set (make-local-variable 'company-backends) '(company-go))
+                          (go-guru-hl-identifier-mode)
                           (company-mode)
                           ))
+(add-hook 'before-save-hook 'gofmt-before-save)
 
 (provide 'init-go)
 
